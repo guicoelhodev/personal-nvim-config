ab1a79810755e569ead87ae67e810df8d28c0cd5
guicoelhodev <gs.coelho_dev@outlook.com>
2024-03-17 12:27:48 -0300

    feat(Telescope): add all telescope default mappings with browser

--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -3,6 +3,24 @@ vim.opt.relativenumber = true
 local opts = { noremap = true, silent = true }
 local keymap = vim.keymap
 
-keymap.set('i', 'jj', '<ESC>', opts)
+-- keymaps
 
-return {}
+keymap.set('i', 'jj', '<ESC>', opts)
+keymap.set('n', '<C-s>', ':w<CR>', opts)
+
+-- telescope remap
+
+local builtin = require 'telescope.builtin'
+vim.keymap.set('n', 'fa', builtin.find_files, { desc = '[F]ind [A]ll files' })
+vim.keymap.set('n', 'fw', builtin.grep_string, { desc = '[F]ind [W]ord' })
+
+-- telescope browser
+
+vim.api.nvim_set_keymap('n', 'ff', ':Telescope file_browser<CR>', opts)
+
+return {
+  {
+    'nvim-telescope/telescope-file-browser.nvim',
+    dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+  },
+}
