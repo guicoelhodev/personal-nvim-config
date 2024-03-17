662e5bb11ecac67e57658df0bd50e68a63382d96
guicoelhodev <gs.coelho_dev@outlook.com>
2024-03-17 19:33:58 -0300

    refactor(mappings): add new mappings and toggle comments position

--- lua/custom/config/keymaps.lua
+++ lua/custom/config/keymaps.lua
@@ -5,16 +5,29 @@ local keymap = vim.keymap
 
 -- keymaps
 
+-- enter on NORMAL mode
 keymap.set('i', 'jj', '<ESC>', opts)
-keymap.set('n', '<C-s>', ':w<CR>', opts)
+
+-- replace content lke Ctrl + f vscode
 keymap.set('n', 'S', ':%s/\\C')
+
+--- delete word and enter on insert mode
 keymap.set('n', 'C', 'ciw')
 
-vim.api.nvim_set_keymap('n', '<leader>d', '<Plug>(coc-definition)', opts) -- go to definition on typescript LSP
+-- go to definition on typescript LSP
+vim.api.nvim_set_keymap('n', '<leader>d', '<Plug>(coc-definition)', opts)
+
+-- close current buffer (file)
 vim.api.nvim_set_keymap('n', '<Space>x', ':bd<CR>', opts)
 
--- lazy git
+-- save file
+keymap.set('n', '<C-s>', ':w<CR>', opts)
 
+-- lazy git
 keymap.set('n', '<leader>g', ':LazyGit<CR>', opts)
+
+-- move with selected block
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv' '>+1<CR>gv=gv'")
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv' '<-2<CR>gv=gv' ")
 
 return {}
