dac8c82129c5ef1fd3a49ceec3e7243e76f3ba2e
guicoelhodev <gs.coelho_dev@outlook.com>
2024-03-24 00:09:01 -0300

    feat(Luasnip|Mappings): add functions macros

--- lua/custom/config/luasnip.lua
+++ lua/custom/config/luasnip.lua
@@ -4,12 +4,16 @@ local t = ls.text_node
 local i = ls.insert_node
 local fmt = require('luasnip.extras.fmt').fmt
 local rep = require('luasnip.extras').rep
+local f = ls.function_node
+local l = require('luasnip.extras').l
 
---- REACT Typescript config
+-----------------------------------------------------------------------------------------
+------------------------------- REACT TYPESCRIPT CONFIG ---------------------------------
+-----------------------------------------------------------------------------------------
 --
 ls.add_snippets('typescriptreact', {
   s(
-    'mkc',
+    'mkc', -- make component
     fmt(
       [[
 import {{ FC }} from 'react';
@@ -31,5 +35,71 @@ export const {}: FC<T{}> = (props) => {{
     )
   ),
 })
+
+ls.add_snippets('typescriptreact', {
+  s(
+    'us', -- create useState
+    fmt(
+      [[
+      const [ {}, set{setter} ] = useState();
+    ]],
+      {
+        i(1, 'state'),
+        setter = l(l._1:sub(1, 1):upper() .. l._1:sub(2, -1), 1),
+      }
+    )
+  ),
+})
+
+ls.add_snippets('typescriptreact', {
+  s(
+    'fn', -- create function
+    fmt(
+      [[
+      const {} = (arg) => {{
+
+      }};
+    ]],
+      {
+        i(1, 'function'),
+      }
+    )
+  ),
+})
+
+ls.add_snippets('typescriptreact', {
+  s(
+    'xfn', -- create export function
+    fmt(
+      [[
+      export const {} = (arg) => {{
+
+      }};
+    ]],
+      {
+        i(1, 'function'),
+      }
+    )
+  ),
+})
+
+ls.add_snippets('typescriptreact', {
+  s(
+    'afn', -- create async function
+    fmt(
+      [[
+      const {} = async (arg) => {{
+
+      }};
+    ]],
+      {
+        i(1, 'function'),
+      }
+    )
+  ),
+})
+
+----------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------
 
 return {}
