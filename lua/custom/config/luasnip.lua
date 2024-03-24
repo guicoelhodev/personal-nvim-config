303d516d43a8b1d2e6f46275dda802ec484c0725
guicoelhodev <gs.coelho_dev@outlook.com>
2024-03-24 00:59:04 -0300

    feat(Luasnip:Test): create mapping test

--- lua/custom/config/luasnip.lua
+++ lua/custom/config/luasnip.lua
@@ -17,39 +17,15 @@ end
 
 ls.add_snippets('typescriptreact', {
   s(
-    'mkc', -- make component
-    fmt(
-      [[
-import {{ FC }} from 'react';
-
-type T{} = {{}};
-
-export const {}: FC<T{}> = (props) => {{
-  return (
-    <div>{}</div>
-  );
-}};
-      ]],
-      {
-        i(1, 'MyComponent'),
-        rep(1),
-        rep(1),
-        rep(1),
-      }
-    )
-  ),
-})
-
-ls.add_snippets('typescriptreact', {
-  s(
     'us', -- create useState
     fmt(
       [[
-      const [ {}, set{setter} ] = useState();
+      const [ {}, set{setter} ] = useState({});
     ]],
       {
         i(1, 'state'),
         setter = l(l._1:sub(1, 1):upper() .. l._1:sub(2, -1), 1),
+        i(2, ''),
       }
     )
   ),
@@ -121,6 +97,23 @@ ls.add_snippets('typescriptreact', {
   ),
 })
 
+ls.add_snippets('typescriptreact', {
+  s(
+    'test', -- create base test
+    fmt(
+      [[
+        it('{}', () => {{
+          
+            const screen = screen(<{} />);
+        }});
+      ]],
+      { i(1, 'test description'), i(2, 'Component') }
+    )
+  ),
+})
+
+------------------------------------------------- CREATE HTML TAGS ------------------------------------------
+
 local commonTags = { 'div', 'section', 'article', 'header', 'footer', 'aside', 'main', 'ul', 'li', 'ol' }
 
 local snippets = {}
@@ -130,6 +123,61 @@ for _, element in ipairs(commonTags) do
 end
 
 ls.add_snippets('typescriptreact', snippets)
+
+------------------------------------------------- LARGE SNIPPETS -----------------------------------------------
+
+ls.add_snippets('typescriptreact', {
+  s(
+    'mkcomp', -- make component
+    fmt(
+      [[
+import {{ FC }} from 'react';
+
+type T{} = {{}};
+
+export const {}: FC<T{}> = (props) => {{
+  return (
+    <div>{}</div>
+  );
+}};
+      ]],
+      {
+        i(1, 'MyComponent'),
+        rep(1),
+        rep(1),
+        rep(1),
+      }
+    )
+  ),
+})
+
+ls.add_snippets('typescriptreact', {
+  s(
+    'mktest', -- make new test
+    fmt(
+      [[
+        import {{ describe, it, expect }} from 'vitest';
+        import {{ render }} from '@testing-library/react';
+        import {{ {} }} from '..';
+
+        describe('Suite test for {} , () => {{
+          it('Should be render correcly', () => {{
+            const screen = screen(<{} />);
+
+            expect(screen.container).toBeInTheDocument()
+          }})
+
+        }})
+      ]],
+      {
+        i(1, 'Component'),
+        rep(1),
+        rep(1),
+      }
+    )
+  ),
+})
+
 ----------------------------------------------------------------------------------------
 ----------------------------------------------------------------------------------------
 
