abf5cd586fb5e7e7de14cb44cbfdcaed29c0d445
guicoelhodev <gs.coelho_dev@outlook.com>
2024-03-23 23:35:04 -0300

    feat(Luasnip): create first snippet to react - Create ts component

--- lua/custom/config/luasnip.lua
+++ lua/custom/config/luasnip.lua
@@ -1,9 +1,35 @@
 local ls = require 'luasnip'
 local s = ls.snippet
 local t = ls.text_node
+local i = ls.insert_node
+local fmt = require('luasnip.extras.fmt').fmt
+local rep = require('luasnip.extras').rep
 
-ls.add_snippets('lua', {
-  s('h', t 'hello word'),
+--- REACT Typescript config
+--
+ls.add_snippets('typescriptreact', {
+  s(
+    'mkc',
+    fmt(
+      [[
+import {{ FC }} from 'react';
+
+type T{} = {{}};
+
+export const {}: FC<T{}> = (props) => {{
+  return (
+    <div>{}</div>
+  );
+}};
+      ]],
+      {
+        i(1, 'MyComponent'),
+        rep(1),
+        rep(1),
+        rep(1),
+      }
+    )
+  ),
 })
 
 return {}
