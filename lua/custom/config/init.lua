79aafece0b33452dcca9ef12e1f06ce0e2111605
guicoelhodev <gs.coelho_dev@outlook.com>
2024-03-28 10:21:36 -0300

    feat: add plugin to see markdown

--- lua/custom/config/init.lua
+++ lua/custom/config/init.lua
@@ -29,4 +29,12 @@ return {
     end,
   },
   { 'sbdchd/neoformat', lazy = false },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  },
 }
