5b968338b8b859d1796930cd9d046c2a5757482b
Guilherme <gs.coelho_dev@outlook.com>
2024-08-19 11:01:35 -0300

    feat(Plugin|Yazi): add new commands to remove file browser

--- lua/custom/config/yazi.lua
+++ lua/custom/config/yazi.lua
@@ -1,10 +1,17 @@
 return {
-	"DreamMaoMao/yazi.nvim",
-	dependencies = {
-		"nvim-telescope/telescope.nvim",
-		"nvim-lua/plenary.nvim",
-	},
-	config = function()
-		vim.keymap.set('n', 'ff', '<CMD>Yazi<CR>')
-	end
+  'mikavilpas/yazi.nvim',
+  event = 'VeryLazy',
+  config = function()
+    local keymap = vim.keymap
+
+    keymap.set('n', 'ff', '<CMD>Yazi<CR>')
+    keymap.set('n', 'fa', '<CMD>Yazi cwd<CR>')
+  end,
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+    keymaps = {
+      show_help = '<f1>',
+    },
+  },
 }
